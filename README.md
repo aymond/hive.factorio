f51be5351578484b35cefa8bed7c5fd7e9ded24e
Aymon Delbridge <aymon.delbridge@gmail.com>
2021-01-22 19:54:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hive.factorio
+
+Setting up factorio to run on Kubernetes cluster.
+
+Read data path: /opt/factorio/data
+Write data path: /factorio
+Binaries path: /opt/factorio/bin
+
---
+++ docker/files/docker-entrypoint.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -eoux pipefail
+
+FACTORIO_VOL=/factorio
+LOAD_LATEST_SAVE="${LOAD_LATEST_SAVE:-true}"
+GENERATE_NEW_SAVE="${GENERATE_NEW_SAVE:-false}"
+SAVE_NAME="${SAVE_NAME:-""}"
+RUNASUSER=factorio
+RUNASGROUP=factorio
+
+mkdir -p "$FACTORIO_VOL"
+mkdir -p "$SAVES"
+mkdir -p "$CONFIG"
+mkdir -p "$MODS"
+mkdir -p "$SCENARIOS"
+mkdir -p "$SCRIPTOUTPUT"
+
+if [[ ! -f $CONFIG/rconpw ]]; then
+  # Generate a new RCON password if none exists
+  pwgen 15 1 >"$CONFIG/rconpw"
+fi
+
+NRTMPSAVES=$( find -L "$SAVES" -iname \*.tmp.zip -mindepth 1 | wc -l )
+if [[ $NRTMPSAVES -gt 0 ]]; then
+  # Delete incomplete saves (such as after a forced exit)
+  rm -f "$SAVES"/*.tmp.zip
+fi
+
+if [[ $(id -u) = 0 ]]; then
+  # Drop to the factorio user
+  SU_EXEC="su-exec ${RUNASUSER}:${RUNASGROUP}"
+else
+  SU_EXEC=""
+fi
+
+NRSAVES=$(find -L "$SAVES" -iname \*.zip -mindepth 1 | wc -l)
+if [[ $GENERATE_NEW_SAVE != true && $NRSAVES ==  0 ]]; then
+    GENERATE_NEW_SAVE=true
+    SAVE_NAME=_autosave1
+fi
+
+if [[ $GENERATE_NEW_SAVE == true ]]; then
+    if [[ -z "$SAVE_NAME" ]]; then
+        echo "If \$GENERATE_NEW_SAVE is true, you must specify \$SAVE_NAME"
+        exit 1
+    fi
+    if [[ -f "$SAVES/$SAVE_NAME.zip" ]]; then
+        echo "Map $SAVES/$SAVE_NAME.zip already exists, skipping map generation"
+    else
+        $SU_EXEC /opt/factorio/bin/x64/factorio \
+            --create "$SAVES/$SAVE_NAME.zip" \
+            --map-gen-settings "$CONFIG/map-gen-settings.json" \
+            --map-settings "$CONFIG/map-settings.json"
+    fi
+fi
+
+FLAGS=(\
+  --port "$PORT" \
+  --server-settings "$CONFIG/server-settings.json" \
+  --server-banlist "$CONFIG/server-banlist.json" \
+  --rcon-port "$RCON_PORT" \
+  --server-whitelist "$CONFIG/server-whitelist.json" \
+  --use-server-whitelist \
+  --server-adminlist "$CONFIG/server-adminlist.json" \
+  --rcon-password "$(cat "$CONFIG/rconpw")" \
+  --server-id /factorio/config/server-id.json \
+)
+
+if [[ $LOAD_LATEST_SAVE == true ]]; then
+    FLAGS+=( --start-server-load-latest )
+else
+    FLAGS+=( --start-server "$SAVE_NAME" )
+fi
+
+# shellcheck disable=SC2086
+exec $SU_EXEC /opt/factorio/bin/x64/factorio "${FLAGS[@]}" "$@"
